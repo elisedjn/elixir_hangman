fc81e2766730793e16b28946338d3f781128ea84
Elise Djn <elisedjn@protonmail.com>
2022-12-04 19:59:32 +0100

    b2 word so far first step

--- b2/lib/b2_web/live/game/word_so_far.ex
+++ b2/lib/b2_web/live/game/word_so_far.ex
@@ -1,15 +1,36 @@
 defmodule B2Web.Live.Game.WordSoFar do
   use B2Web, :live_component
 
+  @states %{
+    initializing: "Type or click on your first guess",
+    good_guess: "Good guess!",
+    bad_guess: "That's not in the word",
+    won: "You won!",
+    lost: "Sorry, you lost...",
+    already_used: "You already picked that letter"
+  }
+
   def mount(socket) do
     {:ok, socket}
   end
 
+  defp state_name(state) do
+    @states[state] || "Unknown state"
+  end
+
   def render(assigns) do
     ~H"""
-      <p>
-        Word So Far
-      </p>
+      <div class="word-so-far">
+        <div class="game-state"><%= state_name(@tally.game_state) %></div>
+        <div class="letters">
+          <%= for ch <- @tally.letters do %>
+            <% cls = if ch != "_", do: "one-letter correct", else: "one-letter" %>
+              <div class={cls}>
+                <%= ch %>
+              </div>
+          <% end %>
+        </div>
+      </div>
     """
   end
 end
